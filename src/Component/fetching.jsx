68228d66c92854595a205c5cc00b7e142951a28a
Harsh <haryan998@gmail.com>
2024-11-28 22:14:41 +0530

    Made some changes

--- src/Component/fetching.jsx
+++ src/Component/fetching.jsx
@@ -1,5 +1,5 @@
 export const fetchPokemon = async () => {
-  const url = "https://pokeapi.co/api/v2/pokemon?limit=12"; // URL to get list of Pokémon
+  const url = "https://pokeapi.co/api/v2/pokemon?limit=12";
 
   const response = await fetch(url);
   if (!response.ok) {
@@ -8,16 +8,15 @@ export const fetchPokemon = async () => {
 
   const data = await response.json();
 
-  // Fetch additional Pokémon data for images
   const promises = data.results.map(async (item) => {
-    const res = await fetch(item.url); // Fetch each Pokémon's detailed info
+    const res = await fetch(item.url);
     const pokemonData = await res.json();
 
     return {
-      id: item.name, // Unique identifier
-      image: pokemonData.sprites.front_default, // Pokémon image
+      id: item.name,
+      image: pokemonData.sprites.front_default,
     };
   });
 
-  return Promise.all(promises); // Wait for all fetches to complete
+  return Promise.all(promises);
 };
